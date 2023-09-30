24e8b2c3569a40818a3c63be69a8364203c9ae22
Adifishy1 <aditya16bhor@gmail.com>
2023-09-30 16:03:16 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    images:{
+      domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+  },
+  }
